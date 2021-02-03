991d7691a98cdfedf7826635c799ba780f5344f5
Tezan Sahu <tezansahu@gmail.com>
2021-02-03 10:53:16 +0530

    initialize app code

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project Athena
+
+A Python-based playground to understand Computer Graphics Curves interactively
---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def main():
+    st.title('Project Athena')
+    st.subheader('A Python-based playground to understand Computer Graphics Curves')
+
+    st.write('_Developed with ❤️ by [Tezan Sahu](https://tezansahu.github.io/)_')
+
+if __name__ == "__main__":
+	main()
